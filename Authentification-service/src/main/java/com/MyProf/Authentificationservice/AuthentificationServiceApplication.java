aa1390def1107d208fa8e3c6d430191bd9872234
MFRADJ <mohamed-fradj@outlook.com>
2024-04-27 12:34:41 +0200

    authentification service created

---
+++ Authentification-service/src/main/java/com/MyProf/Authentificationservice/AuthentificationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.MyProf.Authentificationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthentificationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthentificationServiceApplication.class, args);
+	}
+
+}
